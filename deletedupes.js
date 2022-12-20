66c8f7531b8a8e9589ff27809fa6731f8b35fd61
Kirkulese <44822130+Kirkulese@users.noreply.github.com>
2022-12-20 13:21:35 -0800

    deletedupessaved

---
+++ deletedupes.js
@@ -0,0 +1,16 @@
+var deleteDuplicates = function(head) {
+    let current = head
+   
+    while(current != null && current.next != null){
+        if(current.val == current.next.val){
+            current.next = current.next.next
+
+        }else{
+            current = current.next
+        }
+        
+    }
+
+    return head
+    
+};
