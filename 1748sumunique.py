e714f19850c1c9d9b3255c930774b2884cc441ce
Kirkulese <44822130+Kirkulese@users.noreply.github.com>
2023-02-09 12:43:03 -0800

    1748 python

---
+++ 1748sumunique.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        checkDict = {}
+        total = 0
+        for num in nums:
+            if num not in checkDict:
+                checkDict[num] = True
+                total += num
+            elif num in checkDict and checkDict[num] == True:
+                checkDict[num] = False
+                total -= num
+            else:
+                pass
+
+        return total
